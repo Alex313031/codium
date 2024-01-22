7a4859ae5ae2e181934d29816016fc99ce07dbac
Alexander Frick <Alex313031@gmail.com>
2024-01-21 19:42:15 -0600

    fix nt6 patch

--- nt_6.sh
+++ nt_6.sh
@@ -8,7 +8,7 @@ cd vscode || { echo "'vscode' dir not found"; exit 1; }
 # Make patch
 # git diff > ../nt6.patch
 
-git apply --ignore-whitespace ../nt6.patch  &&
+git apply --reject ../nt6.patch  &&
 
 /usr/bin/find ./ \( -type d -name .git -prune -type d -name node_modules -prune \) -o -type f -name package.json -print0 | xargs -0 sed -i 's/\"\@types\/node\"\:\ \"18\.x\"/\"\@types\/node\"\:\ \"16\.x\"/g' &&
 
