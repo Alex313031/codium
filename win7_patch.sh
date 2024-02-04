e2046fc5386078a5c1b270038a7dcf8a8946ea1c
Alexander Frick <Alex313031@gmail.com>
2024-02-04 03:01:39 -0600

    1.86.0 rebase

---
+++ win7_patch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2129
+
+set -e
+
+cd vscode || { echo "'vscode' dir not found"; exit 1; }
+
+# Make patch
+# git diff > ../nt6.patch
+
+git apply --reject ../nt6.patch  &&
+
+/usr/bin/find ./ \( -type d -name .git -prune -type d -name node_modules -prune \) -o -type f -name package.json -print0 | xargs -0 sed -i 's/\"\@types\/node\"\:\ \"18\.x\"/\"\@types\/node\"\:\ \"16\.x\"/g' &&
+
+cd ..
+
+echo "Patched for Windows NT 6.x!"
